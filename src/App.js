9d226a0bf8f467de0ac2408af1308355823a88ee
David Canonigo <davidcanonigo@davids-mbp.mynetworksettings.com>
2023-04-04 01:19:48 -0400

    using older version of react-router-dom for GH-pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Router } from 'react-router-dom';
 
 import { Navbar } from './components/version2/navbar/Navbar';
 import { Hero } from './components/version2/hero/Hero';
@@ -59,21 +59,25 @@ export default function App() {
     }, [width]);
 
     return (
-        <Router>
-            <>
-                {
-                <ThemeContext.Provider value={[lang, translation]}>
-                    <Navbar screenSize={screenSize} currentPage={currentPage} setCurrentPage={setCurrentPage} setLang={setLang} />
-                    <Routes>
-                        <Route exact path="/portfolio" element={<Hero screenSize={screenSize} />} />
+    <BrowserRouter>
+        <>
+            {
+            <ThemeContext.Provider value={[lang, translation]}>
+                <Navbar screenSize={screenSize} currentPage={currentPage} setCurrentPage={setCurrentPage} setLang={setLang} />
+                        <Route exact path="/portfolio">
+                            <Hero screenSize={screenSize} />
+                        </Route>
                         <Route exact path="/" element={<Hero screenSize={screenSize} />} />
-                        <Route exact path={`/portfolio/${translation.about}`} element={<About screenSize={screenSize} />} />
-                        <Route exact path={`/portfolio/${translation.projects}`} element={<Projects screenSize={screenSize} />} />
-                    </Routes>
-                    <Footer screenSize={screenSize} />
-                </ThemeContext.Provider>
-                }
-            </>
-        </Router>
+                            <Route exact path={`/portfolio/${translation.about}`} >
+                        <About screenSize={screenSize} />
+                        </Route>
+                        <Route exact path={`/portfolio/${translation.projects}`} >
+                            <Projects screenSize={screenSize} />
+                        </Route>
+                <Footer screenSize={screenSize} />
+            </ThemeContext.Provider>
+            }
+        </>
+    </BrowserRouter>
     )
 }
